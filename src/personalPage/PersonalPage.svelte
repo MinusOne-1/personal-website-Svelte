672c0b801dfb69bb93de06343ac782f65af0eac1
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2022-06-24 12:59:50 +0300

    init commit

---
+++ src/personalPage/PersonalPage.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import HeadMenu from "./HeadMenu.svelte"
+  import PersonalInformation from "./PersonalInformation.svelte"
+  import FooterWithLinks from "./FooterWithLinks.svelte"
+
+</script>
+
+
+<template>
+
+  <HeadMenu/>
+  <PersonalInformation/>
+  <FooterWithLinks/>
+</template>
